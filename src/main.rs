af3d7ae273e0950baa2315bb558022eead67bc8f
rahuldev7583 <rahuldev7583@gmail.com>
2026-06-26 01:07:53 +0530

    initial api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/orderbook")]
+async fn get_orderbook() -> impl Responder {
+    HttpResponse::Ok().body("get bids and asks")
+}
+
+#[post("/orders")]
+async fn post_order(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(get_orderbook)
+            .service(post_order)
+            .route("/", web::get().to(HttpResponse::Ok))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
